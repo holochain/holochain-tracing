60dc5b54e288d922d7f30c7f2141d30d65a0d169
David Meister <thedavidmeister@gmail.com>
2020-01-17 18:07:50 +0100

    Release v0.0.13

--- config.nix
+++ config.nix
@@ -63,13 +63,13 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "76be270a21db40fd30234e7e42fd3755e26c66ec";
+  commit = "66ffa7fdb1b3091edfc789b82d05a1799664c8c4";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.12";
+   current = "0.0.13";
    previous = "_._._";
   };
 
